e112cb3ebc6d8d147e288857467422e056bdad3c
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-14 11:54:45 +0100

    feat: horizontal card

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,20 @@
 function renderTeamMembers(array) {
-    let injectedHtml = ``;
-    for (let i = 0; i < teamMembers.length; i++) {
-      let curMemb = teamMembers[i];
-      injectedHtml += `
-           <div class="col-12 col-sm-6 col-md-4 col-lg-3 my-3 ">
-            <div class="card bg-dark text-white">
-              <div class="card-header d-flex justify-content-center">
-               <img src="${curMemb.img}" alt="${curMemb.img}">
-              </div>
-              <div class="card-body">
-                <h3>${curMemb.name}</h3>
-                <p>${curMemb.role}</p>
-                <p>Email: ${curMemb.email}</p>
-              </div>
+  let injectedHtml = ``;
+  for (let i = 0; i < array.length; i++) {
+    let curMemb = array[i];
+    injectedHtml += `
+         <div class="col-12 col-md-6 my-3">
+          <div class="card card-member bg-dark text-white flex-row">
+            <div class="card-image" style="width: 40%; display: flex; align-items: center;">
+             <img src="${curMemb.img}" alt="${curMemb.name}" style="width: 100%; object-fit: cover;">
             </div>
-          </div>`;
-    }
-    cardsMembersElem.innerHTML = injectedHtml;
+            <div class="card-body">
+              <h3>${curMemb.name}</h3>
+              <p>${curMemb.role}</p>
+              <p>Email: ${curMemb.email}</p>
+            </div>
+          </div>
+        </div>`;
   }
-  
+  cardsMembersElem.innerHTML = injectedHtml;
+}
